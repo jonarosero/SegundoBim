24008d2713b567d15a6f8c6111dc8537bd597909
jonarosero <jars9605@hotmail.com>
2021-02-15 15:34:08 -0500

    feat: Services

--- service-course/src/main/java/opencampus/servicecourse/service/CourseService.java
+++ service-course/src/main/java/opencampus/servicecourse/service/CourseService.java
@@ -1,4 +1,18 @@
 package opencampus.servicecourse.service;
 
+import opencampus.servicecourse.repository.entity.Course;
+
+import java.util.List;
+
 public interface CourseService {
+    public List<Course> findCourseAll();
+    public List<Course> findCoursesByEdition(String edition);
+
+    public Course createCourse (Course course);
+    public Course updateCourse (Course course);
+    public Course deleteCourse (Course course);
+    public Course getCourse (Long id);
+    public Course getCourseByCode (String code);
+    public Course getCourseByName (String name);
+
 }
--- service-course/src/main/java/opencampus/servicecourse/service/CourseServiceImpl.java
+++ service-course/src/main/java/opencampus/servicecourse/service/CourseServiceImpl.java
@@ -1,9 +1,83 @@
 package opencampus.servicecourse.service;
 
+import opencampus.servicecourse.repository.CourseRepository;
 import opencampus.servicecourse.repository.entity.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
 import java.util.List;
 
-public class CourseServiceImpl {
+public class CourseServiceImpl implements CourseService
+{
+    @Autowired
+    CourseRepository courseRepository;
 
+    @Override
+    public List<Course> findCourseAll() {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public List<Course> findCoursesByEdition(String edition) {
+        return courseRepository.findByEdition(edition);
+    }
+
+    @Override
+    public Course createCourse(Course course) {
+        Course courseDB = courseRepository.findByCode(course.getCode());
+        if (courseDB != null){
+            return courseDB;
+        }
+
+        course.setStatus("CREATED");
+        course.setEdition("Edición 1");
+        course.setInstitutionName("UTPL");
+        course.setDate(new Date());
+        course = courseRepository.save(course);
+        return courseDB;
+    }
+
+    @Override
+    public Course updateCourse(Course course) {
+        Course courseDB = getCourse(course.getId());
+        if (courseDB == null){
+            return null;
+        }
+        courseDB.setCode(course.getCode());
+        courseDB.setName(course.getName());
+        courseDB.setDescription(course.getDescription());
+        courseDB.setEdition(course.getEdition());
+        courseDB.setDate(course.getDate());
+        courseDB.setHours(course.getHours());
+        courseDB.setCost(course.getCost());
+        courseDB.setTeacher(course.getTeacher());
+        courseDB.setInstitutionName(course.getInstitutionName());
+
+        return courseRepository.save(courseDB);
+    }
+
+    @Override
+    public Course deleteCourse(Course course) {
+        Course courseDB = getCourse(course.getId());
+        if (courseDB == null){
+            return null;
+        }
+        course.setStatus("DELETED");
+        return courseRepository.save(course);
+    }
+
+    @Override
+    public Course getCourse(Long id) {
+        return courseRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Course getCourseByCode(String code) {
+        return courseRepository.findByCode(code);
+    }
+
+    @Override
+    public Course getCourseByName(String name) {
+        return courseRepository.findByName(name);
+    }
 }
