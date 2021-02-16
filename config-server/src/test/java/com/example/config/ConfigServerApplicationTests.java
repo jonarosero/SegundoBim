c625b6b2fca26ed33b94339ed999640d9ff92579
jonarosero <jars9605@hotmail.com>
2021-02-16 18:21:27 -0500

    fix: config-server

--- config-server/src/test/java/com/example/config/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/example/config/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package opencampus.config;
+package com.example.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServiceApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
