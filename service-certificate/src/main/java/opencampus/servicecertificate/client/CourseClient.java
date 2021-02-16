a25a649297488c97f7c2ffb1fb3f7b2dceeffa12
jonarosero <jars9605@hotmail.com>
2021-02-16 00:11:47 -0500

    feat(client): Course and Student clients

---
+++ service-certificate/src/main/java/opencampus/servicecertificate/client/CourseClient.java
@@ -0,0 +1,16 @@
+package opencampus.servicecertificate.client;
+
+import opencampus.servicecertificate.model.Course;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(name = "service-course")
+@RequestMapping("/courses")
+public interface CourseClient {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Course> getCourse(@PathVariable("id") long id);
+}
---
+++ service-certificate/src/main/java/opencampus/servicecertificate/client/StudentClient.java
@@ -0,0 +1,17 @@
+package opencampus.servicecertificate.client;
+
+import opencampus.servicecertificate.model.Student;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(name = "service-student")
+@RequestMapping(value = "/students")
+public interface StudentClient {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable("id") long id);
+
+}
